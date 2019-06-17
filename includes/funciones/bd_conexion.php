456b108f83184afa816814719a8dbc685e547e37
Julk3772 <juan3390@gmail.com>
2019-06-17 12:08:16 -0400

    Haciendo cambios a invitados y corrigiendo solo falta corregir por que no sale el pie

--- includes/funciones/bd_conexion.php
+++ includes/funciones/bd_conexion.php
@@ -1,5 +1,6 @@
 <?php 
     $conn = new mysqli('localhost','root','administrador','gdlwebcamp',3306);
+    $conn->set_charset('utf8');
     if($conn->connect_error){
         echo $error -> $conn->connect_error;
     }
--- invitados.php
+++ invitados.php
@@ -1,36 +1,31 @@
 <?php include_once 'includes/templates/header.php'; ?>
 
-  <section class="seccion contenedor">
-    <h2>Invitados</h2>
-    <?php 
-            ini_set('display_errors', 1);
-            ini_set('display_startup_errors', 1);
-    ?>  
-    <?php
-      try {
-            require_once('includes/funciones/bd_conexion.php');
-            $sql = "SELECT * FROM 'invitados' ";
-            $resultado = $conn->query($sql);
-    } catch (\Exception $e) {
+
+  <?php
+    try{
+      require_once('includes/funciones/bd_conexion.php');
+      $sql = "SELECT * FROM invitados";
+      $resultado = $conn->query($sql);      
+    }catch (Exception $e){
       echo $e->getMessage();
-    }
-    ?>
-    
-   <section class="invitados contenedor seccion">
-        <h2>Nuestros Invitados</h2>
-        <ul class="lista-invitados clearfix">
-        <?php while($invitados = $resultado->fetch_assoc() ){?>
-             <li>
-                <div class="invitado">
-                    <img src="img/<?php echo $invitados['url_imagen'] ?>" alt="imagen invitado">
-                    <p><?php echo $invitado['nombre_invitado'] . "" . $invitado['apellido_invitado'] ?></p>
-                </div>
-             </li>             
-        <?php }?>
-        </ul>
-    </section>                          
-    <?php
-      $conn->close();
-     ?>
-  </section>
+    }    
+  ?>
+  <section class="invitados contenedor seccion">
+    <h2>Nuestros Invitados</h2>
+    <ul class="lista-invitados clearfix">
+    <?php while ($invitados = $resultado->fetch_assoc()) {?>
+      <li>
+        <div class="invitado">
+        <img src="img/<?php echo $invitados['url_imagen'] ?>" alt="imagen invitado">
+        <p><?php echo $invitados['nombre_invitado']. " " .$invitados['apellido_invitado'] ?></p>
+        </div>
+      </li>
+    <?php }?>
+    </ul>
+    </section>
+    <?php 
+      $con->close();
+    ?>  
+ </section>
+ 
 <?php include_once 'includes/templates/footer.php'; ?>
