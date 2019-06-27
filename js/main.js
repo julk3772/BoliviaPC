bd978c77dd66688f2bfb04f98ff812d802916a93
Julk3772 <juan3390@gmail.com>
2019-06-27 11:06:47 -0400

    Al fin se corrigio el error de las pestañas de PROGRMA DEL EVENTO

--- js/main.js
+++ js/main.js
@@ -168,6 +168,7 @@ $(function(){
     $('.programa-evento .info-curso:first').show();
     $('.menu-programa a:first').addClass('activo');
     $('.menu-programa a').on('click', function(){
+        $('.menu-programa a').removeClass('activo');
         $(this).addClass('activo');
         $('.ocultar').hide();
         var enlace = $(this).attr('href');
