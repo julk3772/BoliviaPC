eaa0daf13f64467f3f70550ac876b9345c40a9fe
Julk3772 <juan3390@gmail.com>
2019-07-01 12:51:31 -0400

    Se añadieron la funcion json y modifico validar registro.php

--- validar_registro.php
+++ validar_registro.php
@@ -1,5 +1,23 @@
 <?php include_once 'includes/templates/header.php'; ?>
-<section class="seccion contenedor">
+<section class="seccion contenedor">    
         <h2>Resumen Registro</h2>
+        <?php if(isset($_POST['submit'])): 
+            $nombre = $_POST['nombre'];
+            $apellido = $_POST['apellido'];
+            $email = $_POST['email'];
+            $regalo = $_POST['regalo'];
+            $total = $_POST['total_pedido'];
+            $fecha = date['Y-m-d H:i:s'];
+            //Pedidos
+            $camisas = $_POST['pedido_camisas'];
+            $etiqueta = $_POST['pedido_etiquetas'];
+            $boletos = $_POST['boletos'];
+            include_once 'includes/funciones/funciones.php';
+            $pedido = productos_json($boletos, $camisas, $etiquetas);
+            //eventos
+            $eventos = $_POST['registro'];
+            $registro = eventos_json($eventos);
+        ?>
+        <?php endif; ?>
 </section>        
 <?php include_once 'includes/templates/footer.php'; ?>
