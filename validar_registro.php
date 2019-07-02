1e0ec2b8d9c0bb6256950f4129f03e2c93a63d70
Julk3772 <juan3390@gmail.com>
2019-07-02 13:06:31 -0400

    Clase 343 se añaden registros a la base de datos

--- validar_registro.php
+++ validar_registro.php
@@ -1,23 +1,33 @@
-<?php include_once 'includes/templates/header.php'; ?>
-<section class="seccion contenedor">    
-        <h2>Resumen Registro</h2>
-        <?php if(isset($_POST['submit'])): 
+<?php if(isset($_POST['submit'])): 
             $nombre = $_POST['nombre'];
             $apellido = $_POST['apellido'];
             $email = $_POST['email'];
             $regalo = $_POST['regalo'];
             $total = $_POST['total_pedido'];
             $fecha = date['Y-m-d H:i:s'];
-            //Pedidos
             $camisas = $_POST['pedido_camisas'];
             $etiqueta = $_POST['pedido_etiquetas'];
             $boletos = $_POST['boletos'];
             include_once 'includes/funciones/funciones.php';
             $pedido = productos_json($boletos, $camisas, $etiquetas);
-            //eventos
             $eventos = $_POST['registro'];
             $registro = eventos_json($eventos);
+            try {
+                require_once('includes/funciones/bd_conexion.php');
+                $stmt = $conn->prepare("INSERT INTO registrados (nombre_registrado, apellido_registrado, email_registrado, fecha_registrado, pases_articulos, talleres_registrados, regalo, total_pagado) VALUES (?,?,?,?,?,?,?,?)");
+                $stmt->bind_param("ssssssis", $nombre, $apellido,$email, $fecha, $pedido, $registro, $regalo, $total);
+                $stmt->execute();
+                $stmt->close();
+                $conn->close();
+                header('Location validar_registro.php?exitoso=1');
+        } catch (Exception $e) {
+          echo $e->getMessage();
+        }
         ?>
-        <?php endif; ?>
+        <?php endif; ?>        
+<?php include_once 'includes/templates/header.php'; ?>
+<section class="seccion contenedor">    
+        <h2>Resumen Registro</h2>
+        
 </section>        
 <?php include_once 'includes/templates/footer.php'; ?>
